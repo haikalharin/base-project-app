a1c28350b2c1b94640324a4ea49df187dba5be92
Haikal <haikalharin1998@gmail.com>
2024-08-08 15:39:13 +0700

    init

---
+++ hero/hero.controller.d.ts
@@ -0,0 +1,7 @@
+export declare class HeroController {
+    index(response: any): void;
+    create(response: any): string;
+    store(request: any, response: any): {
+        data: any;
+    };
+}
---
+++ hero/hero.controller.js
@@ -0,0 +1,60 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HeroController = void 0;
+const common_1 = require("@nestjs/common");
+let HeroController = class HeroController {
+    index(response) {
+        response.json({
+            title: 'hero index',
+        });
+    }
+    create(response) {
+        response.cookie('name', 'tobi');
+        return 'hero create';
+    }
+    store(request, response) {
+        return {
+            data: request.body,
+        };
+    }
+};
+exports.HeroController = HeroController;
+__decorate([
+    (0, common_1.Get)('index'),
+    (0, common_1.HttpCode)(200),
+    __param(0, (0, common_1.Res)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], HeroController.prototype, "index", null);
+__decorate([
+    (0, common_1.Get)('create'),
+    __param(0, (0, common_1.Res)({ passthrough: true })),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], HeroController.prototype, "create", null);
+__decorate([
+    (0, common_1.Post)('store'),
+    __param(0, (0, common_1.Req)()),
+    __param(1, (0, common_1.Res)({ passthrough: true })),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, Object]),
+    __metadata("design:returntype", void 0)
+], HeroController.prototype, "store", null);
+exports.HeroController = HeroController = __decorate([
+    (0, common_1.Controller)('hero')
+], HeroController);
+//# sourceMappingURL=hero.controller.js.map
